459de56e03f0552268e64af42b3e28e46df1c930
jawahar273 <jawahar273@gmail.com>
2017-07-23 15:12:01 +0530

    add keras in modules

---
+++ modules/keras/backend/backend_main.py
@@ -0,0 +1,7 @@
+
+
+def first():
+   print( (10 * 10) )
+
+def second():
+    print( ( 20 * 20 ) )
---
+++ modules/keras/main.py
@@ -0,0 +1,12 @@
+
+from applications.app_main import hello
+from backend.backend_main import first, second
+
+
+if __name__ == "__main__":
+   hello()
+   first()
+   second()
+
+
+
